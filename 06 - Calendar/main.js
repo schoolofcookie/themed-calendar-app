aacfb3022f973036401d278c13b8034299cf4e03
David Cooksey Jr <130545391+schoolofcookie@users.noreply.github.com>
2023-05-28 21:13:05 -0700

    Project files added

---
+++ 06 - Calendar/main.js
@@ -0,0 +1,11 @@
+// David Cooksey Jr.
+dycalendar.draw({
+    target: '#dycalendar',
+    type: 'month',
+    dayformat : 'full',
+    monthformat: 'full',
+    highlighttargetdate: false,
+    highlighttoday:true,
+    prevnextbutton: 'show',
+    todaybutton: 'show'
+})
